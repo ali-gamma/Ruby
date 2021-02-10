fe55ae2ca154329a719df4f6fa45c4489e240169
ali-gamma <amultani@gammastack.com>
2021-02-10 18:50:07 +0530

    adding program 3

---
+++ program_3.rb
@@ -0,0 +1,26 @@
+#3. Write a Ruby program to accept a filename from the user and print the extension of that.
+puts "Enter File Name   "
+name = gets.chop.to_s
+
+i = name.index(".")
+if i== 0 
+	puts "No Extension"
+end
+puts "File extension is :  "+name[i..-1]
+
+=begin
+
+Case : 1
+
+Enter File Name   
+readme.txt
+File extension is :  .txt
+
+Case : 2
+
+Enter File Name   
+abc.1123v
+File extension is :  .1123v
+
+=end
+
