e64ee807e090ac4d30793bd33f2213f37399a14d
ali-gamma <78731236+ali-gamma@users.noreply.github.com>
2021-02-16 11:54:24 +0530

    added padding

--- check_everywhere.rb
+++ check_everywhere.rb
@@ -1,6 +1,7 @@
 #1. Write a Ruby program to check whether a given value appears everywhere in a given array. A value is "everywhere" in an array if it presents for #every pair of adjacent elements in the array.
 
 array_1 = []
+
 while true
   puts "enter array elements"
   ele = gets.chop.to_s
@@ -11,15 +12,18 @@ end
 puts "Enter element to search"
 x = gets.chop.to_s
 i = 0
+
 for i in 0 ... array_1.length - 1 
   if array_1[i] != x && array_1[i+1] != x
     puts "The element is not everywhere"
     break		
   end
 end
-if i == array_1.length-2
+
+if i == array_1.length - 2
   puts "Element is everywhere!"
 end	
+
 =begin	
 Case:1
 
